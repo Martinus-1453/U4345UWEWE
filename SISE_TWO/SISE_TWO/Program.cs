6505488c91d70756b835d808d0a75668fac5a320
Martinus-1453 <marvin77@op.pl>
2018-01-05 15:13:27 +0100

    Added SISE2

---
+++ SISE_TWO/SISE_TWO/Program.cs
@@ -0,0 +1,16 @@
+﻿using SISE_TWO;
+using System;
+namespace CodingBackProp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NeuralNet network = new NeuralNet();
+            network.GenerateRandom();
+            network.Run(100000);
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ SISE_TWO/SISE_TWO/Sigmoid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SISE_TWO
+{
+    class Sigmoid
+    {
+        public static double Derivative(double x)
+        {
+            return (1 - x) * x;
+        }
+
+        public static double Func(double x)
+        {
+            return 1 / (1 + Math.Pow(Math.E, -x));
+        }
+    }
+}
